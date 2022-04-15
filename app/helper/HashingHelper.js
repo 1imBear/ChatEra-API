0da1672de30545bf5571faf3f42c398783290d38
1imBear <66860736+1imBear@users.noreply.github.com>
2022-04-15 16:21:44 +0800

    Custom with hashing function
    I deny the bcrypt and start using custom hashing cryptrografy

--- app/helper/HashingHelper.js
+++ app/helper/HashingHelper.js
@@ -1,23 +1,37 @@
 import "regenerator-runtime";
-import bcrypt from "bcrypt";
+//import bcrypt from "bcrypt";
+import { scryptSync, ramdomBytes, randomBytes, timingSafeEqual } from "crypto";
 
-const HashPassword = async (passwd, saltRound = 10) => {
-    try {
-        var salt = await bcrypt.genSalt(saltRound);
-        var hash = await bcrypt.hash(passwd, salt)
-        return hash;
-    } catch (error) {
-        throw error;
-    }
+const HashPassword = (passwd) => {
+    const salt = randomBytes(16).toString('hex');
+    const hash = scryptSync(passwd, salt, 64).toString('hex');
+    return `${salt}:${hash}`;
 }
 
-const ComparePassword = async (ori, passwd) => {
-    try {
-        return bcrypt.compare(passwd, ori);
-    } catch (error) {
-        throw error;
-    }
+const ComparePassword = (hash, passwd) => {
+    const [salt, key] = hash.split(':');
+    const hashBuffer = scryptSync(passwd, salt, 64);
+    const keyBuffer = Buffer.from(key, 'hex');
+    return timingSafeEqual(hashBuffer, keyBuffer);
 }
+
+// const HashPassword = async (passwd, saltRound = 10) => {
+//     try {
+//         var salt = await bcrypt.genSalt(saltRound);
+//         var hash = await bcrypt.hash(passwd, salt)
+//         return hash;
+//     } catch (error) {
+//         throw error;
+//     }
+// }
+
+// const ComparePassword = async (ori, passwd) => {
+//     try {
+//         return await bcrypt.compare(passwd, ori);
+//     } catch (error) {
+//         throw error;
+//     }
+// }
 
 export default {
     HashPassword,
